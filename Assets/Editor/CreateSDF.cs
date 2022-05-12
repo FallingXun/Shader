74fbdec47e3c98a9d21a4eb3b72bf45ccadb74b9
Xun <276919846@qq.com>
2022-05-12 14:47:23 +0800

    SDF

---
+++ Assets/Editor/CreateSDF.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+public class CreateSDF : Editor
+{
+    [MenuItem("SDF/Create")]
+    public static void Create()
+    {
+        var obj = Selection.activeObject;
+        if (obj == null)
+        {
+            return;
+        }
+        var tex = AssetDatabase.LoadAssetAtPath<Texture2D>(AssetDatabase.GetAssetPath(obj));
+        if (tex == null)
+        {
+            return;
+        }
+        Color target = Color.white;
+        int width = tex.width;
+        int height = tex.height;
+        float normal = Mathf.Sqrt(width * width + height * height);
+        bool[,] color = new bool[width, height];
+        float[,] distance = new float[width, height];
+        float dis_min = float.MaxValue;
+        float dis_max = float.MinValue;
+        for (int j = 0; j < height; j++)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                var col = tex.GetPixel(i, j);
+                if (col == target)
+                {
+                    color[i, j] = true;
+                }
+                else
+                {
+                    color[i, j] = false;
+                }
+            }
+        }
+
+        for (int j = 0; j < height; j++)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                if (color[i, j] == false)
+                {
+                    int min = int.MaxValue;
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+                            if (x == i && y == j)
+                            {
+                                continue;
+                            }
+                            if (color[x, y] == false)
+                            {
+                                continue;
+                            }
+                            int dis = (x - i) * (x - i) + (y - j) * (y - j);
+                            if (dis < min)
+                            {
+                                min = dis;
+                                //Debug.LogFormat("({0},{1})({2}, {3}) {4}", i, j, x, y, min);
+                            }
+                        }
+                    }
+                    distance[i, j] = -Mathf.Sqrt(min);
+                    dis_min = Mathf.Min(dis_min, distance[i, j]);
+                    dis_max = Mathf.Max(dis_max, distance[i, j]);
+
+                }
+                else
+                {
+                    int min = int.MaxValue;
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+                            if (x == i && y == j)
+                            {
+                                continue;
+                            }
+                            if (color[x, y] == true)
+                            {
+                                continue;
+                            }
+                            int dis = (x - i) * (x - i) + (y - j) * (y - j);
+                            if (dis < min)
+                            {
+                                min = dis;
+                                //Debug.LogFormat("({0},{1})({2}, {3}) {4}", i, j, x, y, min);
+                            }
+                        }
+                    }
+                    distance[i, j] = Mathf.Sqrt(min);
+                    dis_min = Mathf.Min(dis_min, distance[i, j]);
+                    dis_max = Mathf.Max(dis_max, distance[i, j]);
+                }
+            }
+        }
+
+        var sdf = new Texture2D(width, height);
+        for (int j = 0; j < height; j++)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                distance[i, j] = Mathf.Max(0, distance[i, j] - dis_min);
+                float a = distance[i, j] / (dis_max - dis_min);
+                sdf.SetPixel(i, j, new Color(1, 1, 1, a));
+                //Debug.Log(distance[i, j]);
+            }
+        }
+
+        AssetDatabase.CreateAsset(sdf, string.Format("Assets/SDF/{0}.asset", obj.name));
+    }
+
+
+
+}
+
+
---
+++ Assets/SDF/SDF.shader
@@ -0,0 +1,58 @@
+﻿
+Shader "Custom/SDF.shader"
+{
+	Properties
+	{
+		_MainTex("Main Tex", 2D) = "white" {}
+		_Distance("Distance", Float) = 0
+		_Smooth("Smooth", Float) = 0
+	}
+
+	SubShader
+	{
+		Blend SrcAlpha OneMinusSrcAlpha
+		Pass
+		{
+			CGPROGRAM
+
+			#include "UnityCG.cginc"
+
+			#pragma vertex vert
+			#pragma fragment frag
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float _Distance;
+			float _Smooth;
+
+			struct a2v{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f{
+				float4 pos : SV_POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			v2f vert(a2v i)
+			{
+				v2f o;
+				o.pos = UnityObjectToClipPos(i.vertex);
+				o.uv = i.uv;
+
+				return o;
+			}
+
+			float4 frag(v2f i) : SV_Target
+			{
+				float4 color =  tex2D(_MainTex, i.uv);
+				float distance = color.a;
+				color.a = distance;
+				return float4(1, 1, 1, color.a);
+			}
+
+			ENDCG
+		}
+	}
+}
